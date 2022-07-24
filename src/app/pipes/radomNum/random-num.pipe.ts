207177fcddf7bf96196322d6878a8ea76d6577c5
elazar-gadasi <106330969+elazar-gadasi@users.noreply.github.com>
2022-07-24 19:16:52 +0300

    pipe

--- src/app/pipes/radomNum/random-num.pipe.ts
+++ src/app/pipes/radomNum/random-num.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'randomNum',
 })
 export class RandomNumPipe implements PipeTransform {
-  transform(value: number = 7, args: [number] = [4]): number {
+  transform(value: number, ...args: any[]): number {
     if (args.length < 1) return Math.floor(Math.random() * value);
     if (args.length > 1) return 0;
-    return Math.floor(Math.random() * (value - args[0])) + args[0];
+    return Math.floor(Math.random() * (value - args[0]) + args[0]);
   }
 }
